609b3a67a80b946f8179df50bf76532ba1c9e2aa
Francisco Percara <99842611+Kelisei@users.noreply.github.com>
2022-08-05 18:03:29 -0300

    Update Parcial 2018 octubre.pas

--- CADP/Parcial 2018 octubre.pas
+++ CADP/Parcial 2018 octubre.pas
@@ -1,46 +1,76 @@
-program Zapallo;
-type
-	vectorLetras = array [1..100] of char;
-	letraApariciones = record
-		letra: char;
-		veces: integer;
-	end;
-	vectorContador = array [1..100] of letraApariciones;
 
-//CargarVectorLetras (var V: vectorLetras; var dimL: integer); se dispone
-procedure Contar (V: vectorLetras; dimL: integer);
-var 
-	i, j:integer; actual: char; V2:vectorContador;
-begin
-	i:=1; j:=1;
-	while (i < dimL) do begin
-		V2[j].veces:=0;
-		actual:= V[i];
-		while (i < dimL) and (actual = V[i]) begin 
-			V2[j].veces:=V2[j].veces+1;
-			i:=i+1;
-		end;
-		V2[j].letra:=actual;
-		j:=j+1;
-	end;
-	for i := 1 to j do begin
-		writeln (V2[i].letra, V2[i].veces);
-	end;
-end;
-var
-	V:vectorLetras; dimL:integer;
-begin
-	//CargarVectorLetras (V, dimL);
-	Contar (V, dimL);
-end;
-//Calculo de tiempo
-//2--> asignaciones.
-//while (n+1).(condicion)+n.(cuerpo)-----> (n+1)+n(1+1+ (n+1).(3)+n.(2)+2) 
-//n+1+n(2+3n+3+2n+2)
-//n+1+n(7+5n)
-//n+1+7n+5n^2 
-//5n^2 +8n+1
-//5n^2 +8n+3
-//3.n+2--->for 
-//5n^2 +11n+5
+{Práctico: Se dispone de un vector de caracteres ordenados alfabéticamente (de a lo sumo 100 elementos) que puede 
+aparecer repetidos. Se pide generar un vector que contenga la cantidad total de repeticiones de cada uno de los caracteres 
+alfabéticos. Luego se debe imprimir el vector resultante. }
 
+Program practica2018;
+
+Const 
+  tope = 'z';
+
+Type 
+  vectorLetras = array [1..100] Of char;
+  vectorContador = array ['a'..tope] Of integer;
+
+Procedure ArmarVector (Var vl:vectorLetras; Var diml:integer);
+
+Var 
+  letra: char;
+Begin
+  write('Ingrese el char: ');
+  readln (letra);
+  While (diml<=100)And(letra<>'z') Do
+    Begin
+      diml := diml+1;
+      vl[diml] := letra;
+      write('Ingrese el char: ');
+      readln (letra);
+    End;
+  write ('La diml: ',diml);
+End;
+
+Procedure contar (Var vC:vectorContador; vL:vectorLetras; dimL:integer);
+
+Var 
+  i: integer;
+Begin
+  write ('ENTRAMOS CONTAR');
+  For i:=1 To dimL Do
+    Begin
+      //writeln ('la letra ', vL[i]);
+
+      vC[vL[i]] := vC[vL[i]] + 1;
+      writeln ('la letra ', vL[i], 'aparecio un total de :', vC[vL[i]]);
+    End;
+End;
+
+Procedure iniciarVectorContador(Var vC: vectorContador);
+
+Var 
+  i: char;
+Begin
+  For i:='a' To tope Do
+    vC[i] := 0;
+  writeln('SALIMOS INICIAR VECTOR');
+End;
+Procedure mostrarVect(vc: vectorContador);
+
+Var 
+  i: char;
+Begin
+  For i := 'a' To tope Do
+    writeln ('Letra "',i,'" : ',vc[i]);
+End;
+
+Var 
+  vL: vectorLetras;
+  vC: vectorContador;
+  dl: integer;
+
+Begin
+  dl := 0;
+  ArmarVector(vL,dl);
+  iniciarVectorContador(vC);
+  contar(vC, vL, dl);
+  mostrarVect(vC);
+End.
