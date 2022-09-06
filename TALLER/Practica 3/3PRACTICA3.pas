7544b97aa395a2b02cbc86f510bf8a07a7bbdcfe
Francisco Percara <frankestudios71@gmail.com>
2022-09-06 19:53:20 -0300

    Aviso en la 3

--- TALLER/Practica 3/3PRACTICA3.pas
+++ TALLER/Practica 3/3PRACTICA3.pas
@@ -1,3 +1,9 @@
+//! Aviso: Este codigo no fue actualizado con funciones necesarias, para ver las funciones utiles acá revisar el readme.
+
+
+
+
+
 {3.	Implementar un programa que contenga:
 a. Un módulo que lea información de alumnos de Taller de Programación y almacene en una estructura de datos sólo a aquellos alumnos que posean año de ingreso posterior al 2010. De cada alumno se lee legajo, DNI y año de ingreso. La estructura generada debe ser eficiente para la búsqueda por número de legajo. 
 b. Un módulo que reciba la estructura generada en a. e informe el DNI y año de ingreso de aquellos alumnos cuyo legajo sea inferior a un valor ingresado como parámetro. 
